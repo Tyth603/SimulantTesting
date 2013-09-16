c347e074410aac299f3f87a06dd344159b4976a9
Vagrant <vagrant@ide>
2013-09-16 14:21:10 -0400

    k now comminting with lein stuff

--- project.clj
+++ project.clj
@@ -1,1 +1,3 @@
-[com.datomic/simulant "0.1.6"]
+(defproject SimulantTesting "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/SimulantTesting/test/core.clj
@@ -0,0 +1,6 @@
+(ns SimulantTesting.test.core
+  (:use [SimulantTesting.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
