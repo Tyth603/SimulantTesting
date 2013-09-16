52b8f534b23babc1070541ee3bb61e1373325f5f
Vagrant <vagrant@ide>
2013-09-16 14:32:47 -0400

    commiting more initial stuffs

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject SimulantTesting "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "simulant testing project for Mold-E"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/simulant "0.1.6"]])
